33f25a241cc70e9aafb1c6ceeed9a9a3d6c303a9
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2022-11-01 21:52:39 +0500

    Create laba6t3.3.java

---
+++ laba6t3.3.java
@@ -0,0 +1,18 @@
+import static java.lang.Math.*;
+
+
+public class oliver3 {
+    public static void main(String[] args) {
+        double i,j,k,a = 0,b = 0,c=1;
+        for (i=1;i<=8;i++){
+            for(j=i;j<=2*i;j++){
+                for (k=1;k<=j;k++){
+                    a += (i+pow(j-k,i+j));
+                }
+                b += a;
+            }
+            c *= b;
+        }
+        System.out.println(c);
+    }
+}
