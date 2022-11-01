5849aba6e46e6b538a012bbf6bc98016683a3de8
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2022-11-01 12:09:13 +0500

    Update laba6t3.1.java

--- laba6t3.1.java
+++ laba6t3.1.java
@@ -4,7 +4,6 @@ import static java.lang.Math.*;
 public class oliver3 {
     public static void main(String[] args) {
         double sum=0;
-        Scanner sc = new Scanner(System.in);
         for (int i = 1; i <= 8; i++) {
             for (int j =1; j<=8; j++){
                 sum += pow(j+2*i-1,2);
