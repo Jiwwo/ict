4706c422ac648612cbf971c3b0307c20504ebe07
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2022-11-05 23:50:30 +0500

    Create laba6t5.java

---
+++ laba6t5.java
@@ -0,0 +1,24 @@
+import static java.lang.Math.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double ep1 = 0.0001;
+        double ep2 = 0.001;
+        double ep3 = 0.01;
+        Scanner sc = new Scanner(System.in);
+        double x = sc.nextDouble();
+        System.out.println("xuy1 = " + summa(ep1,x));
+        System.out.println("xuy2 = " + summa(ep2,x));
+        System.out.println("xuy3 = " + summa(ep3,x));
+    }
+    public static double summa(double ep,double x){
+        double s = 0;
+        int n = 1;
+        while (abs(log(x) - 2 * s) > ep){
+            s += pow(x - 1, n)/(n * pow(x + 1, n));
+            n += 2;
+        }
+        return 2*s;
+    }
+}
