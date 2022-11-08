8b9451242338845b4bccc25ce349faad938cb3c4
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2022-11-09 02:01:57 +0500

    Create laba6t8.java

---
+++ laba6t8.java
@@ -0,0 +1,28 @@
+import static java.lang.Math.*;
+import java.util.Scanner;
+
+public class Main {
+    public static class FunctionMy {
+        public static double FunctionMy1(double y){
+            double m = 1;
+            return ((pow(m,2)+2.8*m+0.355)/(cos(2*y) + 3.6));
+        }
+        public static double FunctionMy2(double y){
+            double x = 1, d = 1, a = 1,c = 1;
+            return ((pow(cos(y + sqrt(x)),y)+2.4*d*cbrt(y-x))/(pow(exp(1),y)+pow(exp(1),pow(y,x))*sqrt(abs(a+x*pow(c,4)))));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        while (n < 10){
+            System.out.println("input y");
+            double y = sc.nextDouble();
+            System.out.println(FunctionMy.FunctionMy1(y));
+            System.out.println(FunctionMy.FunctionMy2(y));
+            System.out.println("Continue?");
+            n++;
+        }
+    }
+}
