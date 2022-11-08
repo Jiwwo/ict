f1c5c72fc49a536131d7061629618290d8aaacc2
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2022-11-08 19:26:37 +0500

    Create laba6t7.java

---
+++ laba6t7.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 1; i < 10; i++){
+            for (int j = 1; j < 10; j++){
+                System.out.print(j + "+"+ i +"="+ (i + j)+ "   ");
+            }
+            System.out.println();
+        }
+    }
+}
