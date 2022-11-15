5622e6379ce0f6e142360c94004d64249ef57ec0
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2022-11-15 18:11:09 +0500

    Update laba6t2.java

--- laba6t2.java
+++ laba6t2.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class oliver3 {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double n, p, count = 0, maxdigit = 0;
+        int n; int p; int maxdigit = 0;
+        int count = 0;
         n = sc.nextInt();
-        while (n != 0) {
+        while (n > 0) {
             p = n % 10;
             if (p > maxdigit) {
                 maxdigit = p;
                 count = 0;
             }
             if (p == maxdigit) count++;
-            n /= 10;
+            n = n / 10;
         }
         System.out.println(count);
     }
