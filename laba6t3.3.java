79fc933b81e2719e3b3444a339c9adce8cd0e3b8
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2022-12-01 16:18:20 +0500

    Update laba6t3.3.java

--- laba6t3.3.java
+++ laba6t3.3.java
@@ -1,18 +1,19 @@
 import static java.lang.Math.*;
 
 
-public class oliver3 {
+public class oliver2 {
     public static void main(String[] args) {
-        double i,j,k,a = 0,b = 0,c=1;
+        double i, j, k, a = 0, b = 0, c = 1;
         for (i=1;i<=8;i++){
             for(j=i;j<=2*i;j++){
-                b = 0;
                 for (k=1;k<=j;k++){
                     a += (i+pow(j-k,i+j));
                 }
                 b += a;
+                a = 0;
             }
             c *= b;
+            b = 0;
         }
         System.out.println(c);
     }
