59bce1669f8188c5011c69d3f25862bc25a1b533
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2022-12-15 16:36:27 +0500

    Update t8.java

--- laba8/t8.java
+++ laba8/t8.java
@@ -6,22 +6,25 @@ public class Main {
         System.out.println("input n");
         int n = in.nextInt();
         int [][] arr = new int[n][n];
-        FillTheBlock(arr,0,0,n/2 - 1,n/2 - 1);
-        FillTheBlock(arr,0,n/2,n - 1,n/2 - 1);
-        FillTheBlock(arr,n/2,0,n/2 - 1,n - 1);
-        FillTheBlock(arr,n/2,n/2,n - 1,n - 1);
+        int k_max = (n * n - n)/2 + n;
+        FillTheBlock(arr,0,0,n/2 - 1,n/2 - 1,k_max);
+        FillTheBlock(arr,0,n/2,n - 1,n/2 - 1,k_max);
+        FillTheBlock(arr,n/2,0,n/2 - 1,n - 1,k_max);
+        FillTheBlock(arr,n/2,n/2,n - 1,n - 1,k_max);
         PrintArr(arr);
     }
-    public static void FillTheBlock(int [][] arr,int x, int y, int m, int end_x){
+    public static void FillTheBlock(int [][] arr,int x, int y, int m, int end_x,int k_max){
         int k = 1;
         for (int lp = 0,c = x; lp < m; lp++,c++){
-            for (int i = y + lp; i < m - lp; i++){
+            if (k >= k_max) break;
+            for (int i = y + lp; i <= m - lp - 1; i++){
                 arr[i][c] = k++;
             }
             for (int l = m - 3 * lp, b = x + 2 * lp; l >= y + lp; l--, b++){
                 arr[l][b] = k++;
             }
-            for (int h = end_x - 3 * lp - 1,h1 = y + lp; h >= x + 1 ; h--){
+            for (int h = end_x - 2 * lp - 1,h1 = y + lp; h >= x + 1 + lp ; h--){
+                arr[h1][h] = k++;
             }
         }
     }
