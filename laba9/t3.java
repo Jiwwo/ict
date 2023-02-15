1c903c4ba02b25778edde3a861e9812be1c94f06
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2023-02-15 22:28:40 +0500

    Update t3.java

--- laba9/t3.java
+++ laba9/t3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class App{
+public class Main{
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             int cnt1 = 0, cnt3 = 0;
@@ -30,6 +30,15 @@ public class App{
                 ||charArr[i] == '?' && charArr[i] == charArr[i + 1]
                 ||charArr[i] == ':' && charArr[i] == charArr[i + 1]
                 ||charArr[i] == ';' && charArr[i] == charArr[i + 1]) System.out.println("arr contains .. or ,, or !! etc.");
+            }
+            char [] charArr1 = {'1','2','3','4','5','6','7','8'};
+            for (int i = 0; i < charArr1.length - 1; i++){
+                for(int j = 0; j < charArr1.length - 1; j++){
+                    for(int k = 0; k < charArr1.length - 1; k++){
+                        if ((charArr1[i] > charArr1[k] && charArr1[i + 1] > charArr1[k]) && (charArr1[j] < charArr1[k] && charArr1[j + 1] < charArr1[k]))
+                        System.out.println("i j k that fullfill the condition " + i + " " + j + " " + k);
+                    }
+                }
             }
         }
     }
