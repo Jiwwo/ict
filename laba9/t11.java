fd38f6d46d8afd2a61bf184f22d84734e4716df4
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2023-05-04 15:22:49 +0500

    Update t11.java

--- laba9/t11.java
+++ laba9/t11.java
@@ -2,15 +2,15 @@ import java.util.Arrays;
 
 public class class_c1 {
     public static void main(String[] args) {
-        String[] strings = {" everybody ", "  everybody  ", "  go to hell  "};
+        String[] strings = {"   Everybody  ", " everybody    ", "   go   to hell   "};
         for (int i = 0; i < strings.length; i++) {
             strings[i] = strings[i].trim();
         }
         for (int i = 0; i < strings.length; i++) {
-            strings[i] = strings[i].replaceAll("o", "a");
+            strings[i] = strings[i].replaceAll("\s+", " ");
         }
         for (int i = 0; i < strings.length; i++) {
-            strings[i] = strings[i].replaceAll("\s", "-");
+            strings[i] = strings[i].substring(0, 1).toUpperCase() + strings[i].substring(1);
         }
         System.out.println(Arrays.toString(strings));
     }
