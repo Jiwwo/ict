f015da43d7b8c4227f3930130c892ba01c1a1b50
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2023-05-13 03:40:31 +0500

    Create t4.java

---
+++ laba10/t4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file1 = new File("C:\\Users\\ivanx\\OneDrive\\Рабочий стол\\t4.txt");
+        FileWriter fwr = new FileWriter(file1, true);
+        BufferedReader fread = new BufferedReader(new FileReader(file1));
+        String regular = "([а-яА-Я]*)|([a-zA-Z]*)|(\\W*)([_.,;:!?])(\\w*)";
+        fwr.write("\nOUT\n");
+        while (fread.ready()){
+            String line = fread.readLine();
+            String[] array = line.split("\\s+");
+            for (String str : array) {
+                if (str.matches(regular)) {
+                    fwr.write(str + "\n");
+                }
+            }
+        }
+        fwr.flush();
+        fwr.close();
+    }
+}
