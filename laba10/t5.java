8b756485d6a15bab0aa9bac4c712a190657e7901
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2023-06-03 16:00:10 +0500

    /

--- laba10/t5.java
+++ laba10/t5.java
@@ -2,20 +2,21 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\ivanx\\OneDrive\\Рабочий стол\\51.txt");
-        File file1 = new File("C:\\Users\\ivanx\\OneDrive\\Рабочий стол\\52.txt");
-        FileWriter fwr = new FileWriter(file1, true);
-        BufferedReader fread = new BufferedReader(new FileReader(file));
-        String str = "(.*[A-Z][a-z]+.*[A-Z]+.*)|(.*[А-Я][а-я]+.*[А-Я]+.*)";
-        String line = fread.readLine();
-        String [] array = line.split("\\.\\s");
-        for (int i = 0; i < array.length; i++){
-           if (array[i].matches(str)){
-               fwr.write(" " + array[i] + ".");
-           }
+        File file1 = new File("C:\\Users\\ivanx\\OneDrive\\Рабочий стол\\t5.1.txt");
+        File file2 = new File("C:\\Users\\ivanx\\OneDrive\\Рабочий стол\\t5.2.txt");
+        appendSentences(file1, file2);
+    }
+    public static void appendSentences(File file1, File file2) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(file1));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file2, true));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] sentences = line.split("[.!?]\\s+");
+            for (String sentence : sentences) {
+                if (sentence.matches(".*[A-Z][a-z]*\\s+[A-Z]{2,}.*")) {
+                    writer.write(sentence + ". ");
+                }
+            }
         }
-        fwr.flush();
-        fwr.close();
     }
 }
-
